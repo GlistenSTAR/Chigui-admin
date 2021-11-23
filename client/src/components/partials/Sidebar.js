7c8ff096a806848f8debed761a1aa80418f81db8
GlistenSTAR <glistenstar00@gmail.com>
2021-11-23 20:38:26 +0100

    add free sevices on sidebar and add global valuables

--- client/src/components/partials/Sidebar.js
+++ client/src/components/partials/Sidebar.js
@@ -1,7 +1,20 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import PropTypes from "prop-types";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBatteryHalf, faBolt, faBook, faBorderAll, faCar, faOilCan, faReceipt, faSignOutAlt, faThumbsUp, faUserAlt, faWrench} from "@fortawesome/free-solid-svg-icons";
+import {
+    faBatteryHalf,
+    faBolt,
+    faBook,
+    faBorderAll,
+    faCar,
+    faOilCan,
+    faReceipt,
+    faSignOutAlt,
+    faThumbsUp,
+    faTools,
+    faUserAlt,
+    faWrench
+} from "@fortawesome/free-solid-svg-icons";
 import {connect} from "react-redux";
 import {logoutUser} from "../../actions/authActions";
 import {Link} from "react-router-dom";
@@ -18,17 +31,29 @@ class Sidebar extends Component {
         return (
             <div className="border-right h-100" id="sidebar-wrapper">
                 <div className="list-group list-group-flush">
-                    <Link to="/dashboard" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faBorderAll}/>Dashboard</Link>
-                    <Link to="/users" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faUserAlt}/>Users</Link>
-                    <Link to="/electronic" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faBolt}/>Electronic</Link>
-                    <Link to="/battery" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faBatteryHalf}/>Battery</Link>
+                    <Link to="/dashboard" className="list-group-item list-group-item-action"><FontAwesomeIcon
+                        icon={faBorderAll}/>Dashboard</Link>
+                    <Link to="/users" className="list-group-item list-group-item-action"><FontAwesomeIcon
+                        icon={faUserAlt}/>Users</Link>
+                    <Link to="/electronic" className="list-group-item list-group-item-action"><FontAwesomeIcon
+                        icon={faBolt}/>Electronic</Link>
+                    <Link to="/battery" className="list-group-item list-group-item-action"><FontAwesomeIcon
+                        icon={faBatteryHalf}/>Battery</Link>
                     <Link to="/car" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faCar}/>Car</Link>
-                    <Link to="/review" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faBook}/>Review</Link>
-                    <Link to="/oil" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faOilCan}/>Oil</Link>
-                    <Link to="/highlight" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faThumbsUp}/>Highlight</Link>
-                    <Link to="/quote" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faReceipt}/>Quote</Link>
-                    <Link to="/services" className="list-group-item list-group-item-action"><FontAwesomeIcon icon={faWrench}/>Services</Link>
-                    <Link className="list-group-item list-group-item-action" onClick={this.onLogoutClick} to="#"><FontAwesomeIcon icon={faSignOutAlt} />Logout </Link>
+                    <Link to="/review" className="list-group-item list-group-item-action"><FontAwesomeIcon
+                        icon={faBook}/>Review</Link>
+                    <Link to="/oil" className="list-group-item list-group-item-action"><FontAwesomeIcon
+                        icon={faOilCan}/>Oil</Link>
+                    <Link to="/highlight" className="list-group-item list-group-item-action"><FontAwesomeIcon
+                        icon={faThumbsUp}/>Highlight</Link>
+                    <Link to="/quote" className="list-group-item list-group-item-action"><FontAwesomeIcon
+                        icon={faReceipt}/>Quote</Link>
+                    <Link to="/services" className="list-group-item list-group-item-action"><FontAwesomeIcon
+                        icon={faWrench}/>Services</Link>
+                    <Link to="/free_services" className="list-group-item list-group-item-action"><FontAwesomeIcon
+                        icon={faTools}/>Free Services</Link>
+                    <Link className="list-group-item list-group-item-action" onClick={this.onLogoutClick}
+                          to="#"><FontAwesomeIcon icon={faSignOutAlt}/>Logout </Link>
                 </div>
             </div>
         );
@@ -46,5 +71,5 @@ const mapStateToProps = state => ({
 
 export default connect(
     mapStateToProps,
-    { logoutUser }
+    {logoutUser}
 )(Sidebar);
